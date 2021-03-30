9777fec6fe9aba0738749047e5b8b1c4c69290aa
Sairo Guanipa <sairojgg@gmail.com>
2021-03-30 15:32:20 -0600

    Added log in and log out functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,14 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
   end
 
   def show
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
   def create
