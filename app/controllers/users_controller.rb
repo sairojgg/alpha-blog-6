3980379d2fd806d323f67f817a181fcee38b51d5
Sairo Guanipa <sairojgg@gmail.com>
2021-03-30 15:39:56 -0600

    Added session logic to the app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up."
       redirect_to articles_path
     else
