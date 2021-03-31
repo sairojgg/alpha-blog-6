e4431f03c72a5c7c971f72e40b8395991f754f89
Sairo Guanipa <sairojgg@gmail.com>
2021-03-31 17:53:03 -0600

    Added category creation with its functional test

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,26 @@
+class CategoriesController < ApplicationController
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def index
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
