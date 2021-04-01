76e10237798a35b28c402b24e779aae134a74500
Sairo Guanipa <sairojgg@gmail.com>
2021-03-31 18:12:01 -0600

    Added index view and integration test for categories

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -12,4 +12,15 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Sports", response.body
   end
+
+  test "reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count', 1 do
+      post categories_path, params: { category: { name: " " } }
+    end
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+    assert_match "errors", response.body
+  end
 end
